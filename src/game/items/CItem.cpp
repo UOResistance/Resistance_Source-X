36a1f1029a2f3dd372a3a4bc5aba8dc84d54e6a3
Jhobean <frederic.gag@gmail.com>
2025-01-18 21:53:06 -0500

    Les trap invisible sont vue rouge aussi quand elles sont invisibles

--- src/game/items/CItem.cpp
+++ src/game/items/CItem.cpp
@@ -1997,7 +1997,7 @@ HUE_TYPE CItem::GetHueVisible() const
 	{
 		if (IsAttr(ATTR_INVIS))
 		{
-			if (!IsType(IT_SPAWN_CHAR) && !IsType(IT_SPAWN_ITEM))  //Spawn point always keep their m_wHue (HUE_RED_DARK)
+			if (!IsType(IT_SPAWN_CHAR) && !IsType(IT_SPAWN_ITEM) && !IsType(IT_TRAP))  //Spawn point always keep their m_wHue (HUE_RED_DARK)
 				return g_Cfg.m_iColorInvisItem;
 		}
 	}
